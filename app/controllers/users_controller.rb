7879cf518b6eb0258276ee808903ce2fdba52c95
jpdf00 <j.pdf00@gmail.com>
2021-02-18 14:27:39 -0300

    Add linters, readme, license

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -31,6 +31,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:email,:password,:username)
+    params.require(:user).permit(:email, :password, :username)
   end
 end
